382d73fecef8aee3349d7640a3a294d4de433f00
Estácio Junior <estaciojr@cruz-online.com>
2021-12-20 14:59:52 -0300

    Atualizando

---
+++ Dalla_Eventos/src/br/com/dalla/hrc/actions/IncluirSam.java
@@ -0,0 +1,11 @@
+package br.com.dalla.hrc.actions;
+
+import br.com.sankhya.extensions.actionbutton.AcaoRotinaJava;
+import br.com.sankhya.extensions.actionbutton.ContextoAcao;
+
+public class IncluirSam implements AcaoRotinaJava {
+
+    public void doAction(ContextoAcao ctx) throws Exception {
+
+    }
+}
